6da493a48f2296a17dd7240417fea8f220a7463e
dodo <eg4rang@naver.com>
2025-08-21 23:27:57 +0900

    Feat: 스크립트 칩 컴포넌트 구현

--- app/src/main/java/com/snacks/nuvo/ui/theme/NuvoTypography.kt
+++ app/src/main/java/com/snacks/nuvo/ui/theme/NuvoTypography.kt
@@ -414,6 +414,20 @@ data class NuvoTypography(
             trim = LineHeightStyle.Trim.None,
         )
     ),
+    val interRegular13: TextStyle = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal,
+        fontSize = 13.sp,
+        lineHeight = 13.sp,
+        letterSpacing = 0.sp,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        ),
+        lineHeightStyle = LineHeightStyle(
+            alignment = LineHeightStyle.Alignment.Center,
+            trim = LineHeightStyle.Trim.None,
+        )
+    ),
 )
 
 val LocalTypography = staticCompositionLocalOf { NuvoTypography() }
