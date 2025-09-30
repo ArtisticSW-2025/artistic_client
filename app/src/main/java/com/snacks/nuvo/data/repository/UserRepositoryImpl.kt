49a1515424c6c477944720a20ddf6de7eed4d1f9
dodo <eg4rang@naver.com>
2025-09-30 22:32:42 +0900

    Fix: User DataSource/Repository에 싱글톤 어노테이션 제거

--- app/src/main/java/com/snacks/nuvo/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/snacks/nuvo/data/repository/UserRepositoryImpl.kt
@@ -2,9 +2,7 @@ package com.snacks.nuvo.data.repository
 
 import com.snacks.nuvo.data.datasource.UserDataSource
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
 class UserRepositoryImpl @Inject constructor(
     private val userDataSource: UserDataSource,
 ) : UserRepository {
